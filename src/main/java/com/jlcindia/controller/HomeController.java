041d87e96601f7bfa7723cc75cd636120b4da0e9
shashikant00 <shashikantgit10@gmail.com>
2024-12-31 11:19:02 +0530

    initial commit

---
+++ src/main/java/com/jlcindia/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.jlcindia.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/show")
+    public String msg(){
+       return  "Welcome to Springboot project";
+    }
+}
